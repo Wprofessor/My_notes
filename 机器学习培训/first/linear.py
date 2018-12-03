5534ee8fb1729435cb91b155899b32e059ce69cb
Wprofessor <wprofessor@gmail.com>
2018-12-03 19:43:08 +0800

    Wprofessor commit.

--- 机器学习培训/first/linear.py
+++ 机器学习培训/first/linear.py
@@ -1,12 +1,15 @@
+from builtins import len, int, range
+
 import numpy as np
 from model import Linear
-from model import Multinomial
+from model import multinomialModel
+from model import LWRL
+from model import StereMultinomial
 # 数据可视化
 import matplotlib.pyplot as plt
 
-
 def load_data():
-    path = "./data/linear_data.txt"
+    path = "./data/temperature.txt"
     file = open(path)
     str = file.read()
     raw_data = str.split("，")
@@ -28,17 +31,31 @@ def linear(X, y):
     model.draw(X, y)
 
 
-def multinomial(X, y):
-    model = Multinomial()
-    model.train(X, y, 1e-5, step=10000)
+def MultinomialModel(X, y):
+    model = multinomialModel()
+    model.train(X, y,5000,1e-7,5)
     model.draw(X, y)
 
+def lwrl(X, y):
+    model = LWRL()
+    preds = []
+    for i in range(len(X)):
+        pred = model.predict(X[i], X, y, k=1)
+        preds.append(pred)
+    model.draw(X, y, preds)
 
+def stereMultinomialModel(X, y):
+    model = StereMultinomial()
+    model.train(X, y, 500000, 20, 1e-5)
+    model.draw(X, y)
 if __name__ == "__main__":
     int_data = load_data()
     print(int_data)
     draw(int_data)
     print(int_data)
     # linear(range(len(int_data)), int_data)
-    x = range(len(int_data))
-    multinomial(x, int_data)
+    # x = range(len(int_data))
+    x = np.arange(start=1, stop=len(int_data) + 1, step=1)
+    # MultinomialModel(x, int_data)
+    # lwrl(x,int_data)
+    stereMultinomialModel(x,int_data)
