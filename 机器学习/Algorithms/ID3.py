48d80b590e2a53931ecbc1d42b8bb9ed1330b6f1
Wprofessor <wprofessor@gmail.com>
2019-02-16 22:50:08 +0800

    Wprofessor commit.

--- 机器学习/Algorithms/ID3.py
+++ 机器学习/Algorithms/ID3.py
@@ -1,14 +1,27 @@
 from math import log
+import pickle
 
 
 def creatDataset():
-    dataset = [[1, 1, 'yes'],
-               [1, 1, 'yes'],
-               [1, 0, 'no'],
-               [0, 1, 'no'],
-               [0, 1, 'no']]
-    labels = ['no surfacing', 'flippers']
-    return dataset, labels
+    # 数据集
+    dataSet = [[0, 0, 0, 0, 'no'],
+               [0, 0, 0, 1, 'no'],
+               [0, 1, 0, 1, 'yes'],
+               [0, 1, 1, 0, 'yes'],
+               [0, 0, 0, 0, 'no'],
+               [1, 0, 0, 0, 'no'],
+               [1, 0, 0, 1, 'no'],
+               [1, 1, 1, 1, 'yes'],
+               [1, 0, 1, 2, 'yes'],
+               [1, 0, 1, 2, 'yes'],
+               [2, 0, 1, 2, 'yes'],
+               [2, 0, 1, 1, 'yes'],
+               [2, 1, 0, 1, 'yes'],
+               [2, 1, 0, 2, 'yes'],
+               [2, 0, 0, 0, 'no']]
+    # 分类属性
+    labels = ['年龄', '有工作', '有自己的房子', '信贷情况']
+    return dataSet, labels
 
 
 # 计算给定数据的经验熵
@@ -50,7 +63,7 @@ def splitDataSet(dataSet, axis, value):
 
 
 # 根据信息增益选择最好的数据集划分方式
-def chooseSplit(dataSet):
+def chooseSplit(dataset):
     num = len(dataset[0]) - 1
     ok = 0.0
     count = -1  # 标记最好的特征的索引
@@ -87,17 +100,76 @@ def ensureClassify(classList):
 
 # 创建决策树(递归)
 def creatTree(dataSet, labels):
-    classList = [example[-1] for example in dataset]
+    classList = [example[-1] for example in dataSet]
     # 如果类别相同，停止继续划分
     if classList.count(classList[0]) == len(classList):
         return classList[0]
+    # 如果数据集已经处理了所有属性，但分类依然不唯一，则采取多数表决的方式确定分类
+    if len(dataSet[0]) == 1:
+        return ensureClassify(classList)
+    bestFest = chooseSplit(dataSet)  # 得到最优特征的索引
+    bestFestLabel = labels[bestFest]
+    # 用字典的结构建树
+    myTree = {bestFestLabel: {}}
+    del (labels[bestFest])  # 删除当前最优特征
+    # 注意：del方法删除的是变量，而不是数据
+    # a=1       对象 1 被 变量a引用，对象1的引用计数器为1
+    # b=a        对象1 被变量b引用，对象1的引用计数器加1
+    # c=a       1对象1 被变量c引用，对象1的引用计数器加1
+    # del a     删除变量a，解除a对1的引用
+    # del b     删除变量b，解除b对1的引用
+    # print(c)  最终变量c仍然引用1
+    # li=[1,2,3,4,5]  列表本身不包含数据1,2,3,4,5，而是包含变量：li[0] li[1] li[2] li[3] li[4]
+    # first=li[0]     拷贝列表，也不会有数据对象的复制，而是创建新的变量引用
+    # del li[0]
+    # print(li)      输出[2, 3, 4, 5]
+    # print(first)   输出 1
+    featValue = [example[bestFest] for example in dataSet]  # 得到最优特征的所有属性值
+    uniqueVals = set(featValue)  # 去重
+    for value in uniqueVals:
+        subLabels = labels[:]  # 复制特征种类
+        # 递归建树
+        # 使用嵌套字典构造树结构
+        myTree[bestFestLabel][value] = creatTree(splitDataSet(dataSet, bestFest, value), subLabels)
+    return myTree
+    # 构造的字典大概是这样
+    # {'no surfacing': {0: 'no', 1: {'flippers': {0: 'no', 1: 'yes'}}}}
 
 
-dataset = [[1, 1, 'yes'],
-           [1, 1, 'yes'],
-           [1, 0, 'no'],
-           [0, 1, 'no'],
-           [0, 1, 'no']]
-for i in range(len(dataset[0]) - 1):
-    fealist = [example[i] for example in dataset]
-    print(fealist)
+# 使用决策树进行分类(递归)
+# testFeat：样例特征
+def classify(inputTree, Labels, testFeat):
+    firstStr = list(inputTree.keys())[0]  # 表示根节点
+    secondDict = inputTree[firstStr]  # 表示根节点的儿子
+    featIndex = Labels.index(firstStr)  # 标签的索引
+    for key in secondDict.keys():
+        if testFeat[featIndex] == key:
+            # 如果不是叶子节点，递归继续遍历
+            if type(secondDict[key]) == dict:
+                classLable = classify(secondDict[key], Labels, testFeat)
+            else:
+                classLable = secondDict[key]
+    return classLable
+
+
+# 将决策树保存在硬盘上
+def storeTree(inputTree, filename):
+    fw = open(filename, 'wb+')
+    pickle.dump(inputTree, fw)
+    fw.close()
+
+
+# 读取保存的决策树的内容
+def grabTree(filename):
+    fr = open(filename, 'rb')
+    return pickle.load(fr)
+
+
+dataSet, labels = creatDataset()
+tree = creatTree(dataSet, labels)
+print(tree)
+# 将对象序列化为string形式，而不是存入文件中。
+# storeTree(tree, 'classifyTree.txt')
+print(grabTree('classifyTree.txt'))
+dataSet, labels = creatDataset()
+print(classify(tree, labels, [0, 1, 0, 0]))
--- 机器学习/Algorithms/test.py
+++ 机器学习/Algorithms/test.py
@@ -1,16 +1,8 @@
-import numpy as np
+def tt(x):
+    del (x[2])
+    return x
 
-fr = open("./Data/knn_data.txt")
-num = len(fr.readlines())
-returnMat = np.zeros((num, 2),dtype=float)
 
-fr = open("./Data/knn_data.txt")
-count = 0
-dataLabels = []
-for line in fr.readlines():
-    line = line.strip()
-    line = line.split()
-    returnMat[count, :] = line[0:2]
-    dataLabels.append(line[2])
-    count += 1
-
+x = [1, 2, 3]
+tt(x)
+print(x)
