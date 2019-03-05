585141c32ed177a795e05183148bf8213eb2012f
Wprofessor <wprofessor@gmail.com>
2019-03-05 22:01:26 +0800

    Wprofessor commit.

---
+++ 机器学习/Algorithms/svmSimplify.py
@@ -0,0 +1,26 @@
+# 加载数据
+import random
+
+
+def loadDataSet(fileName):
+    dataSet = []
+    Labels = []
+    fr = open(fileName)
+    for line in fr.readline():
+        # 去掉换行符
+        lineArr = line.strip()
+        # 默认逗号隔开
+        lineArr = lineArr.split()
+        dataSet.append([float(lineArr[0]), float(lineArr[1])])
+        Labels.append(lineArr[2])
+    return dataSet, Labels
+
+
+# 随机选择和第一个alpha不一样的另一个alpha下标
+# i:第一个alpha的下标；m：全部alpha的数量
+def selectA(i, m):
+    j = i
+    while i == j:
+        j = int(random.uniform(0, m))
+    return j
+
