082255085105bcc39fec1abba91f7733e0b16701
Wprofessor <wprofessor@gmail.com>
2019-03-09 21:34:29 +0800

    Wprofessor commit.

--- 机器学习/Algorithms/svmSimplify.py
+++ 机器学习/Algorithms/svmSimplify.py
@@ -1,13 +1,14 @@
 # 加载数据
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def loadDataSet(fileName):
     dataSet = []
     Labels = []
     fr = open(fileName)
-    for line in fr.readline():
+    for line in fr.readlines():
         # 去掉换行符
         lineArr = line.strip()
         # 默认逗号隔开
@@ -36,6 +37,23 @@ def judgeAlpha(aj, H, L):
     return aj
 
 
+def show(dataSet, Labels):
+    # 正样本
+    data_plus = []
+    # 负样本
+    data_minus = []
+    for i in range(len(dataSet)):
+        if Labels[i] > 0:
+            data_plus.append(dataSet[i])
+        else:
+            data_minus.append(dataSet[i])
+        data_plus_np = np.array(data_plus)
+        data_minus_np = np.array(data_minus)
+        plt.scatter(np.transpose(data_plus_np)[0], np.transpose(data_plus_np)[1])
+        plt.scatter(np.transpose(data_minus_np)[0], np.transpose(data_minus_np)[1])
+        plt.show()
+
+
 # 简化版smo优化算法
 def smoSimple(dataSet, Labels, C, toler, maxIter):
     # mat意思为转换为矩阵
@@ -45,11 +63,104 @@ def smoSimple(dataSet, Labels, C, toler, maxIter):
     b = 0
     m, n = np.shape(dataSet)
     # 初始化alpha
-    alpha = np.mat(np.zeros((m, 1)))
+    alphas = np.mat(np.zeros((m, 1)))
     # 记录alpha不改变时遍历的次数
     iter = 0
     while iter < maxIter:
         # 记录alpha是否被优化
         alphaChanged = 0
         for i in range(m):
-           
+            # 计算Ei
+            fxi = float(np.multiply(alphas, Labels).T * (dataSet * dataSet[i, :].T)) + b
+            Ei = fxi - float(Labels[i])
+            if (Labels[i] * Ei < -toler) and (alphas[i] < C) or (Labels[i] * Ei > toler) and (alphas[i] > 0):
+                # 选取另一个alpha
+                j = selectA(i, m)
+                fxj = float(np.multiply(alphas, Labels).T * (dataSet * dataSet[j, :].T)) + b
+                Ej = fxj - float(Labels[j])
+                # copy()和直接赋值=的区别
+                # a=[0,1,2,3,4,5]
+                # b=a
+                # c=a.copy()
+                #
+                # del a[1]
+                # '''
+                #    各变量值为：
+                #    a=[0, 2, 3, 4, 5]
+                #    b=[0, 2, 3, 4, 5]
+                #    c=[0, 1, 2, 3, 4, 5]
+                # '''
+                #
+                # b.remove(4)
+                # '''
+                #    各变量值为：
+                #    a=[0, 2, 3, 5]
+                #    b=[0, 2, 3, 5]
+                #    c=[0, 1, 2, 3, 4, 5]
+                # '''
+                #
+                # c.append(9)
+                # '''
+                #    各变量值为：
+                #    a=[0, 2, 3, 5]
+                #    b=[0, 2, 3, 5]
+                #    c=[0, 1, 2, 3, 4, 5, 9]
+                # '''
+                alphaIold = alphas[i].copy()
+                alphaJold = alphas[j].copy()
+                # 计算上界和下界
+                if Labels[i] != Labels[j]:
+                    L = max(0, alphas[j] - alphas[i])
+                    H = min(C, C + alphas[j] - alphas[i])
+                else:
+                    L = max(C, alphas[i] + alphas[j] - C)
+                    H = min(C, alphas[i] + alphas[j])
+                if L == H:
+                    print("L == H")
+                    # 本次循环结束，直接开始下次循环
+                    continue
+                # 计算分母2K(x1,x2) - K(x1,x1)-K(x2,x2)
+                eta = 2.0 * (dataSet[i, :] * dataSet[j, :].T) - \
+                      dataSet[i, :] * dataSet[i, :].T - dataSet[j, :] * dataSet[j, :].T
+
+                # 因为a初始值为0，所以此条件是防止a过小的一个优化操作
+                if eta >= 0:
+                    print("eta >= 0")
+                    continue
+                # 修改a2
+                alphas[j] -= Labels[j] * (Ei - Ej) / eta
+                # 修剪a2
+                alphas[j] = judgeAlpha(alphas[j], H, L)
+                if abs(alphaJold - alphas[j]) < 0.5:
+                    print("更新的太小了")
+                    continue
+                # 更新a1
+                alphas[i] += (Labels[i] * Labels[j]) * (alphaJold - alphas[j])
+                # 计算b1
+                b1 = b - Ei - Labels[i] * (dataSet[i, :] * dataSet[i, :].T) * (alphas[i] - alphaIold) - \
+                     Labels[j] * (dataSet[j, :] * dataSet[i, :].T) * (alphas[j] - alphaJold)
+                # 计算b2
+                b2 = b - Ej - Labels[i] * (dataSet[i, :] * dataSet[j, :].T) * (alphas[i] - alphaIold) - \
+                     Labels[j] * (dataSet[j, :] * dataSet[j, :].T) * (alphas[j] - alphaJold)
+                # 选取合适的b
+                if alphas[i] > 0 and alphas[i] < C:
+                    b = b1
+                elif alphas[j] > 0 and alphas[j] < C:
+                    b = b2
+                else:
+                    b = (b1 + b2) / 2.0
+                alphaChanged += 1
+                print("第%d次迭代 样本:%d, alpha优化次数:%d" % (iter, i, alphaChanged))
+
+        if alphaChanged == 0:
+            iter += 1
+        else:
+            iter = 0
+        print("迭代次数:----------------------------------------------", iter)
+    return b, alphas
+
+
+dataSet, Labels = loadDataSet('./Data/svmtestset.txt')
+Labels = list(map(float, Labels))
+b, alphas = smoSimple(dataSet, Labels, 0.6, 0.001, 40)
+show(dataSet, Labels)
--- 机器学习/Algorithms/test.py
+++ 机器学习/Algorithms/test.py
@@ -1,7 +1,51 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-a = [1, 2, 3, 4, 5]
-a = np.mat(np.array(a)).transpose()
-m = 5
-alpha = np.mat(np.zeros((m, 1)))
-print(alpha)
+
+def loadDataSet(fileName):
+    dataSet = []
+    Labels = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        # 去掉换行符,默认逗号隔开
+        lineArr = line.strip().split('\t')
+        dataSet.append([float(lineArr[0]), float(lineArr[1])])
+        Labels.append(lineArr[2])
+    return dataSet, Labels
+
+
+dataset, Labels = loadDataSet('./Data/svmtestset.txt')
+dataset = np.mat(dataset)
+
+# print(np.multiply(alpha, Labels))
+# m, n = np.shape(dataset)
+Labels = list(map(float, Labels))
+
+
+# Labels = np.mat((Labels)).transpose()
+# alpha = np.mat(np.zeros((m, 1)))
+# front = np.multiply(alpha, Labels).T
+# data = dataset * dataset[0, :].T
+# print(front.shape)
+# print(data.shape)
+# print(data * front)
+def show(dataSet, Labels):
+    # 正样本
+    data_plus = []
+    # 负样本
+    data_minus = []
+    for i in range(len(dataSet)):
+        if Labels[i] > 0:
+            data_plus.append(dataSet[i])
+        else:
+            data_minus.append(dataSet[i])
+    data_plus_np = np.array(data_plus)
+    data_minus_np = np.array(data_minus)
+    plt.scatter(np.transpose(data_plus_np)[0], np.transpose(data_plus_np)[1])
+    plt.scatter(np.transpose(data_minus_np)[0], np.transpose(data_minus_np)[1])
+    plt.show()
+    # print(data_plus_np)
+    # print(data_minus_np.transpose())
+
+
+show(dataset, Labels)
