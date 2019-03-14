a8163d314e8a0eca051b93d2c9a2d8b9ccb38562
Wprofessor <wprofessor@gmail.com>
2019-03-14 20:05:44 +0800

    Wprofessor commit.

--- ACM/HDU2102(三维BFS).cpp
+++ ACM/HDU2102(三维BFS).cpp
@@ -2,8 +2,8 @@
 using namespace std;
 const int INF = 0x3f3f3f3f;
 int c,n,m,t;
-char map1[2][10][10];
-int d[2][10][10];	//距离起始点的最短距离
+char map1[2][15][15];
+int d[2][15][15];	//距离起始点的最短距离
 int dx[] = {0,1,0,-1};
 int dy[] = {1,0,-1,0};
 int sz,sx,sy;	//起始点
@@ -13,8 +13,7 @@ struct node {
 	int y;
 	int z;
 };
-bool ok;	//标记骑士会不会死 
-void BFS() {
+bool BFS() {
 	queue<node> que;
 	//	初始化最短距离
 	for(int i = 0; i < 2; i++) {
@@ -33,8 +32,8 @@ void BFS() {
 	while(que.size()) {
 		node temp = que.front();
 		que.pop();
-		if(temp.z == gz && temp.x == gx && temp.y == gy)
-			break;
+		if(temp.z == gz && temp.x == gx && temp.y == gy && d[gz][gx][gy] <= t)
+			return true;
 		for(int i = 0; i < 4; i++) {
 			int nx = temp.x + dx[i];
 			int ny = temp.y + dy[i];
@@ -42,54 +41,55 @@ void BFS() {
 				node ent;
 				if(map1[temp.z][nx][ny] == '#') {	//如果是时空穿梭机
 					ent.z =  (temp.z == 0 ? 1 : 0);
-					d[ent.z][nx][ny] = d[temp.z][temp.x ][temp.y];
-					if(map1[ent.z][nx][ny] == '*'){
-						ok = false;
-						return ;
-					}
-					ent.x = nx;
-					ent.y = ny;
-					que.push(ent);
-				} else {
 					d[temp.z][nx][ny] = d[temp.z][temp.x ][temp.y] + 1;
+					if(d[ent.z][nx][ny] == INF) {
+						d[ent.z][nx][ny] = d[temp.z][nx][ny];
+						ent.x = nx;
+						ent.y = ny;
+						que.push(ent);
+					}
+				} else {
+					d[temp.z][nx][ny] = d[temp.z][temp.x][temp.y] + 1;
 					ent.z = temp.z;
 					ent.x = nx;
 					ent.y = ny;
 					que.push(ent);
 				}
 			}
-
 		}
 	}
+	return false;
 }
 int main() {
 	while(scanf("%d",&c) != EOF) {
 		for(int i = 0; i < c; i++) {
-			ok = true;
 			cin>>n>>m>>t;
+			sx = 0;
+			sy = 0;
+			sz = 0;
 			for(int j = 0; j < 2; j++) {
 				for(int k = 0; k < n; k++) {
 					cin>>map1[j][k];
 					for(int l = 0; l < m; l++) {
-						if(map1[j][k][l] == 'S') {
-							sz = j;
-							sx = k;
-							sy = l;
-						}
 						if(map1[j][k][l] == 'P') {
 							gz = j;
 							gx = k;
 							gy = l;
 						}
+						if(j == 1) {
+							if((map1[0][k][l] == '#' && map1[1][k][l] == '#') || (map1[0][k][l] == '#' && map1[1][k][l] == '*') || (map1[0][k][l] == '*' && map1[1][k][l] == '#') ) {
+								map1[0][k][l] = '*';
+								map1[1][k][l] = '*';
+							}
+						}
 					}
 				}
 			}
+			if(BFS()) {
+				cout<<"YES"<<endl;
+			} else
+				cout<<"NO"<<endl;
 		}
-		BFS();
-		if(d[gz][gx][gy] <= t)
-			cout<<"YES"<<endl;
-		else
-			cout<<"NO"<<endl;
 	}
 	return 0;
 }
