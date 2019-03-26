63027626d67f0f7b8cf0968b8117b06690d3a236
Wprofessor <wprofessor@gmail.com>
2019-03-26 21:13:21 +0800

    Wprofessor commit.

--- 机器学习/Algorithms/regression.py
+++ 机器学习/Algorithms/regression.py
@@ -1,3 +1,28 @@
 # 线性回归（标准方程法）
 import numpy as np
 
+
+def loadDataSet(filename):
+    # numFeat = len(open(filename).readline().split())
+    dataSet = []
+    Labels = []
+    for line in open(filename).readlines():
+        dataSet.append([float(line.split()[0]), float(line.split()[1])])
+        Labels.append(float(line.split()[-1]))
+    return dataSet, Labels
+
+
+def standRegres(xArr, yArr):
+    xMat = np.mat(xArr)
+    yMat = np.mat(yArr).T   # 先转换成n行一列
+    temp = (xMat.T * xMat)
+    if np.linalg.det(temp) == 0.0:  # 判断行列式是否为0
+        print("行列式为0，无法计算")
+        return
+    else:
+        w = temp.I * xMat.T * yMat
+    return w
+
+
+dataSet, Labels = loadDataSet("./Data/ex0.txt")
+print(standRegres(dataSet, Labels))
--- 机器学习/NumpyArrayMatrix.py
+++ 机器学习/NumpyArrayMatrix.py
@@ -8,14 +8,16 @@ print(np.dot(a, b))
 # [[37 40]
 #  [85 92]]
 
-# np.vdot() 对应元素相乘求和
+# 对应元素相乘求和
+# np.vdot()
 a = np.array([[1, 2], [3, 4]])
 b = np.array([[11, 12], [13, 14]])
 # vdot 将数组展开计算内积
 print(np.vdot(a, b))
 # 1*11 + 2*12 + 3*13 + 4*14 = 130
 
-# numpy.inner() fanhui一维向量内积
+# 返回一维向量内积
+# numpy.inner()
 print(np.inner(np.array([1, 2, 3]), np.array([0, 1, 0])))
 # 等价于 1*0+2*1+3*0
 
@@ -24,7 +26,8 @@ a = np.array([[1, 2], [3, 4]])
 print(np.linalg.det(a))
 # -2.0
 
-# numpy.linalg.inv() 计算n阶矩阵的逆
+# 计算n阶矩阵的逆
+# numpy.linalg.inv()
 x = np.array([[1, 2], [3, 4]])
 y = np.linalg.inv(x)
 print(x)
